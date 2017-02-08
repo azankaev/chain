0566c71f2557012e9f680150fbbf2f40a5aff891
Sergey Vorvulev <svorvulev@cinimex.ru>
2017-02-08 18:27:25 +0300

    7

--- chaincode_finished.go
+++ chaincode_finished.go
@@ -80,7 +80,7 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 	var key, value string
 	var err error
 	fmt.Println("running write()")
-logger.Debug("write logger.Debug ----------------------------------------")
+logger.Infof("write logger.Infof ----------------------------------------")
 	if len(args) != 2 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
 	}
@@ -102,7 +102,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
-logger.Debug("read logger.Debug ----------------------------------------")
+logger.Infof("read logger.Infof ----------------------------------------")
 	key = args[0]
 	valAsbytes, err := stub.GetState(key)
 	if err != nil {
